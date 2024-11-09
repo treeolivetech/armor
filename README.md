5497abf99df3691ea961d487fdb925e0b48cbeba
wakskevin <wakskevin@outlook.com>
2024-11-09 14:55:46 +0300

    included python script for setting up the aliases

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# BashAliases
-A collection of useful bash aliases for various tools and frameworks
+# Manage Bash Aliases
+
+Script for managing the .bash_aliases file - create or overwrite the .bash_aliases file with a set of predefined aliases for various tasks.
+
+## Features
+
+- **Create or Overwrite .bash_aliases:** Checks if the .bash_aliases file exists and provides an option to overwrite it.
+
+- **Alias Management:** Adds a predefined set of aliases for updating, editing, and refreshing the .bash_aliases file, along with aliases for repositories, Django, PostgreSQL, Nginx, and more.
+
+## Setup
+
+1. Ensure Python is installed in your system
+
+2. Clone the repository:
+
+    ```bash
+    git clone https://github.com/your-username/manage-bash-aliases.git
+    ```
+
+3. Change directory
+
+    ```bash
+    cd manage-bash-aliases
+    ```
+
+4. Run the Python script:
+
+    ```bash
+    python manage_bash_aliases.py
+    ``` 
---
+++ bash_aliases.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+
+# Read the aliases from the text file
+aliases_file_path = Path("aliases.txt")
+if aliases_file_path.exists():
+    bash_aliases_content = aliases_file_path.read_text()
+else:
+    raise FileNotFoundError(f"{aliases_file_path} not found.")
+
+# Define the path to the .bash_aliases file
+file_path = Path.home() / ".bash_aliases"
+
+# Check if the .bash_aliases file exists and handle accordingly
+if file_path.exists():
+    overwrite = (
+        input(
+            "The ~/.bash_aliases file already exists. Do you want to overwrite it? (y (yes) / n (no)): "
+        )
+        .strip()
+        .lower()
+    )
+    if overwrite == "y" or overwrite == "yes":
+        file_path.write_text(bash_aliases_content)
+        print("The ~/.bash_aliases file has been overwritten.")
+    else:
+        print("The ~/.bash_aliases file was not overwritten.")
+else:
+    file_path.write_text(bash_aliases_content)
+    print("The ~/.bash_aliases file has been created.")
