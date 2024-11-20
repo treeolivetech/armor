1f420d81d322913f660cc5fe23cb595aa154ff43
wakskevin <kevin.wasike@lox-informatics.com>
2024-11-20 19:57:29 +0000

    switched from inquirer to @inquirer/prompts npm package.

--- index.mjs
+++ index.mjs
@@ -1,49 +1,44 @@
 #!/usr/bin/env node
 
 import chalk from "chalk";
-import inquirer from "inquirer";
+import { select } from "@inquirer/prompts";
 import generateSSHKey from "./generateSSHKey/index.mjs";
 import manageBashAliases from "./manageBashAliases/index.mjs";
 
 async function main() {
-  const questions = [
-    {
-      type: "list",
-      name: "utility",
+  try {
+    const utility = await select({
       message: "What task would you like to perform?",
       choices: [
         { name: "Manage Bash Aliases", value: "manageBashAliases" },
         { name: "Generate SSH Keys", value: "generateSSHKey" },
         { name: "Exit", value: "exit" },
       ],
-      default: "exit",
-    },
-  ];
+    });
 
-  const answers = await inquirer.prompt(questions);
+    switch (utility) {
+      case "manageBashAliases":
+        await manageBashAliases();
+        break;
 
-  switch (answers.utility) {
-    case "manageBashAliases":
-      await manageBashAliases();
-      break;
+      case "generateSSHKey":
+        await generateSSHKey();
+        break;
 
-    case "generateSSHKey":
-      await generateSSHKey();
-      break;
+      case "exit":
+        console.log(chalk.yellow("Exiting the utility. Goodbye!"));
+        break;
 
-    case "exit":
-      console.log(chalk.yellow("Exiting the utility. Goodbye!"));
-      break;
+      default:
+        console.log(chalk.red("Invalid choice."));
+        break;
+    }
 
-    default:
-      console.log(chalk.red("Invalid choice."));
-      break;
+    process.exit(0);
+  } catch (error) {
+    console.error(chalk.red(`An error occurred: ${error.message}`));
+    process.exit(1);
   }
-
-  process.exit(0);
 }
 
-main().catch((error) => {
-  console.error(chalk.red(`An error occurred: ${error.message}`));
-  process.exit(1);
-});
+main();
